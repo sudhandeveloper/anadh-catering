d57d8156a099aaea4e245ef78900b0fd530b9206
sudhandeveloper <gmsudhan3@gmail.com>
2023-10-21 11:02:17 +0530

    new

--- anandth-catering-main/src/App.js
+++ anandth-catering-main/src/App.js
@@ -1,9 +1,14 @@
-
-import './App.css';
-
+import { Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/a-home";
 function App() {
   return (
- <></>
+    <>
+      {" "}
+      <Routes>
+        <Routes path="/" Element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
