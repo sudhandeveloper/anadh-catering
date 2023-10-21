3c0307f7ff8a8904142b0da8990a9f94b5458437
sudhandeveloper <gmsudhan3@gmail.com>
2023-10-21 15:45:51 +0530

    new

--- anandth-catering-main/src/App.js
+++ anandth-catering-main/src/App.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import Navbar from "./components/header-and-footer/header";
 import Home from "./pages/a-home";
 function App() {
   return (
     <>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- anandth-catering-main/src/pages/a-home.jsx
+++ anandth-catering-main/src/pages/a-home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <section>
-        <h2 className="text-[200px]">sbbssiudbius</h2>
+        <h2 className="text-[200px]"></h2>
       </section>
     </>
   );
