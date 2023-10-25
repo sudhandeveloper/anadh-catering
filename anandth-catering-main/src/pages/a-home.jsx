4b7018da2b77940c949cc6eb7ffce97cf3465139
sudhandeveloper <gmsudhan3@gmail.com>
2023-10-25 15:10:57 +0530

    commite

--- anandth-catering-main/src/pages/a-home.jsx
+++ anandth-catering-main/src/pages/a-home.jsx
@@ -27,11 +27,56 @@ const Home = () => {
         </div>
       </section>
 
-      <section>
-        <div className="w-full h-full">
+  {/* ================== MEET KERALA'S BEST INTERIOR DESIGNERS ================ */}
 
-        </div>
-      </section>
+  <section>
+          <div className="pt-10 pb-1 bg-gray-200">
+            <div className="">
+              <h1 className="text-center max-md:text-[50px] text-gray-500 font-bold uppercase text-[45px] font-cormantThiner ">
+                About <span className="text-red-600"> Inddecore Interio </span>
+              </h1>
+              <center>
+                <div className="h-1 bg-red-600 rounded-full w-96 max-md:hidden"></div>
+              </center>
+            </div>
+            <div className="flex w-full h-auto px-20 mt-20 mb-20 max-md:px-6 max-lg:flex-col ">
+              <div className="w-[50%] max-lg:w-full h-[500px] Baner-black-white">
+                <img
+                  className="object-cover w-full h-full"
+                  src={ImageThree}
+                  alt="loading"
+                />
+              </div>
+
+              <div className="w-[50%] max-lg:w-full  h-auto ">
+                <div className="h-[30%] text-center pt-5">
+                  <h1 className="text-center text-gray-500 font-bold uppercase text-[45px] font-cormantThiner ">
+                    <span className="text-red-600"> Inddecore </span>Interio
+                  </h1>
+                </div>
+
+                <div className="w-full h-[70%] px-16 py-16 max-sm:px-6 bg-white ">
+                  <h2 className="uppercase tracking-widest text-[22px] text-gray-500">
+                    MEET KERALA'S BEST
+                  </h2>
+                  <p className="text-[32px] tracking-wider font-medium text-red-600">
+                    INTERIOR DESIGNERS
+                  </p>
+                  <p className="text-justify font-extrabold text-[16px] text-gray-800  font-cormantThiner">
+                    stablished in the year 2018, THOMAR is a highly reputed
+                    interior brand. It is supported by a powerful team of
+                    architects, interior designers and supervisors etc. This
+                    widely acclaimed brand, which offers turnkey interior
+                    solutions, is known for its exceptional research and
+                    development wing. It handles both residential and commercial
+                    projects. It maintains international standards in design,
+                    material, as well as execution..
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
     </>
   );
 };
