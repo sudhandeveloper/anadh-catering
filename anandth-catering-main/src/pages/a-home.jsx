a115be403bf7b360211c50e4362eb4aee9e89daa
sudhandeveloper <gmsudhan3@gmail.com>
2023-10-27 16:46:33 +0530

    comite

--- anandth-catering-main/src/pages/a-home.jsx
+++ anandth-catering-main/src/pages/a-home.jsx
@@ -301,33 +301,41 @@ const Home = () => {
               <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
                 <img
                   className="object-cover w-full dark:bg-gray-500 aspect-square"
+                  alt="loading"
                   src="https://source.unsplash.com/random/300x300/?1"
                 />
                 <img
                   className="object-cover w-full dark:bg-gray-500 aspect-square"
+                  alt="loading"
                   src="https://source.unsplash.com/random/300x300/?2"
                 />
                 <img
+                  alt="loading"
                   className="object-cover w-full dark:bg-gray-500 aspect-square"
                   src="https://source.unsplash.com/random/300x300/?3"
                 />
                 <img
+                  alt="loading"
                   className="object-cover w-full dark:bg-gray-500 aspect-square"
                   src="https://source.unsplash.com/random/300x300/?4"
                 />
                 <img
+                  alt="loading"
                   className="object-cover w-full dark:bg-gray-500 aspect-square"
                   src="https://source.unsplash.com/random/300x300/?1"
                 />
                 <img
                   className="object-cover w-full dark:bg-gray-500 aspect-square"
+                  alt="loading"
                   src="https://source.unsplash.com/random/300x300/?2"
                 />
                 <img
+                  alt="loading"
                   className="object-cover w-full dark:bg-gray-500 aspect-square"
                   src="https://source.unsplash.com/random/300x300/?3"
                 />
                 <img
+                  alt="loading"
                   className="object-cover w-full dark:bg-gray-500 aspect-square"
                   src="https://source.unsplash.com/random/300x300/?4"
                 />
