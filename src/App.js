e57ffb61372a9df62860475a3d4290f977bb0c62
sudhandeveloper <gmsudhan3@gmail.com>
2023-10-27 17:23:42 +0530

    cot

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/header-and-footer/header";
 import Footer from "./components/header-and-footer/fotter";
 import Home from "./pages/a-home";
 import Aboutus from "./pages/about-us";
+import Services from "./pages/service";
 import Contactss from "./pages/contact";
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<Aboutus />} />
+        <Route path="/Service" element={<Services />} />
         <Route path="/Contact" element={<Contactss />} />
       </Routes>
       <Footer />
