23eea89bd2b6a0f2be2e9372067ca2ca03f6ce27
sudhandeveloper <gmsudhan3@gmail.com>
2023-10-30 16:59:15 +0530

    comite

--- src/components/menus-component/menu-compo/menu-compo.jsx
+++ src/components/menus-component/menu-compo/menu-compo.jsx
@@ -72,8 +72,68 @@ const Menu = () => {
           </Tab>
 
           <Tab label="Salads">
-            <div>
-              <table></table>
+          <div>
+              <section className="py-6 ">
+                <div className="container flex flex-col  p-4 mx-auto">
+                  <div className="flex">
+                    {" "}
+                    <div className="h-10 mr-5 w-[7px] mt-2 bg-green-700 "> </div>{" "}
+                    <div> </div>
+                    <h1 className="text-start text-[40px] mb-5 text-green-800 font-extrabold">
+                      OUR FANTASTIC BEVERAGES
+                    </h1>{" "}
+                  </div>
+
+                  <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2">
+                    <div className="border-[7px] border-green-700 shadow-md pl-5 pb-5 text-start shadow-gray-600 rounded-md">
+                      <h2 className="text-green-700 mt-5 font-cormantThiner   uppercase text-[25px] font-extrabold ">
+                        {" "}
+                        Welcome Drink
+                      </h2>
+                      <ul className="text-[20px] font-extrabold list-decimal pl-5 mt-5">
+                        <li>Fresh Lime</li>
+                        <li> Jal –Jeera</li>
+                        <li>Rose Sarbat </li>
+                        <li>KhusSarbat Cool </li>
+                        <li>Drinks Soda </li>
+                        <li> (Sweet or Salted) </li>
+                        <li> Mint – O – Pine Panagam </li>
+                      </ul>
+                    </div>
+
+                    <div className="border-[7px] border-green-700 shadow-md pl-5 pb-5 text-start shadow-gray-600 rounded-md">
+                      <h2 className="text-green-700 mt-5 font-cormantThiner   uppercase text-[25px] font-extrabold ">
+                        {" "}
+                        Fresh Fruit Juice
+                      </h2>
+                      <ul className="text-[20px] font-extrabold list-decimal pl-5 mt-5">
+                        <li>Pineapple Juice</li>
+                        <li> Orange Juice</li>
+                        <li>Mosambi Juice</li>
+                        <li>Grape Juice</li>
+                        <li>Water-Melon Juice</li>
+                       
+                      </ul>
+                    </div>
+
+                    <div className="border-[7px] border-green-700 shadow-md pl-5 pb-5 text-start shadow-gray-600 rounded-md">
+                      <h2 className="text-green-700 mt-5 font-cormantThiner   uppercase text-[25px] font-extrabold ">
+                        {" "}
+                        Mocktails
+                      </h2>
+                      <ul className="text-[20px] font-extrabold list-decimal pl-5 mt-5">
+                        <li>Fruit Punch</li>
+                        <li> Tender Delight</li>
+                        <li>Blue Moon</li>
+                        <li>Mojito</li>
+                        <li>Silver Spring</li>
+                        <li>Virgin Mary</li>
+                        <li>Cranberry Cutie</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </section>
             </div>
           </Tab>
 
